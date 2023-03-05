86acc09c5f02155c9addf03aa1f6595848509f27
Thinh Nguyen <tthinh089@gmail.com>
2023-03-05 23:34:10 +0700

    update waiting page

--- src/pages/waiting.js
+++ src/pages/waiting.js
@@ -7,7 +7,12 @@ export default function Waiting() {
       <form>
         <div class="title">
           <img src="https://res.cloudinary.com/dyajk5rfe/image/upload/v1677771930/logo_im5hov.png" />
-          <h1>Please wait for the admin to set your role and department!</h1>
+          <h1>
+            Please wait for the admin to set your role and department.
+            <br />
+            Come back later !
+          </h1>
+          <button>Logout current account</button>
         </div>
         <div class="dot-typing"></div>
       </form>
