908930a658eed52857de44ae33b3d4440c5a141e
Nem <namvu2839@gmail.com>
2023-04-18 16:57:27 +0700

    update

---
+++ redux/Slices/Topic.js
@@ -0,0 +1,46 @@
+import { apiIndex } from "./index";
+
+const extendedApi = apiIndex.injectEndpoints({
+  endpoints: (build) => ({
+    getTopics: build.query({
+      query: () => ({
+        url: "topic",
+        method: "GET",
+      }),
+      providesTags: ["Topic"],
+    }),
+    addTopic: build.mutation({
+      query: (data) => ({
+        url: "topic",
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["Topic"],
+    }),
+    editTopic: build.mutation({
+      query: (data) => ({
+        url: `topic/${data.id}`,
+        method: "PUT",
+        body: data,
+      }),
+      invalidatesTags: ["Topic"],
+    }),
+    deleteTopic: build.mutation({
+      query: (id) => ({
+        url: `topic/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Topic"],
+    }),
+  }),
+  overrideExisting: false,
+});
+
+export const {
+  useGetTopicsQuery,
+  useAddTopicMutation,
+  useEditTopicMutation,
+  useDeleteTopicMutation,
+} = extendedApi;
+
+
